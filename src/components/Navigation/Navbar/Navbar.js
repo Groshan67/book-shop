0989bf4a5211c0704aff9bfa98cd0a4ea5b49b1d
Groshan67 <ghassemroshan@gmail.com>
2023-07-09 09:16:26 +0330

    First commit

---
+++ src/components/Navigation/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavItem from "../NavItems/NavItems"
+import "./Navbar.css";
+
+const Navbar = (props) => {
+  return (
+    <header className="navbar">
+     <NavItem></NavItem>
+    </header>
+  );
+};
+
+export default Navbar;
